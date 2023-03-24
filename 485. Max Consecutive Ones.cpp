4901004bb2f369988e30bf1033edac8d2c598b41
Pranav Sood <99885598+pranav-sood-12@users.noreply.github.com>
2023-03-25 02:00:15 +0530

    Create 485. Max Consecutive Ones.cpp

---
+++ 485. Max Consecutive Ones.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans=-1,count=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            count++;
+            else
+            {
+                ans=max(ans,count);
+                count=0;
+
+            }
+
+        }
+                ans=max(ans,count);
+
+        return ans;
+    }
+};
