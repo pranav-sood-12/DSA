e1dac254e51eaf025eb8e8d7c2d0b5a00316b352
Pranav Sood <99885598+pranav-sood-12@users.noreply.github.com>
2023-06-02 22:18:23 +0530

    Create 2712. Minimum Cost to Make All Characters Equal.cpp
    contest 347

---
+++ string/2712. Minimum Cost to Make All Characters Equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long minimumCost(string s) {
+        long long ans=0;
+        int n=s.size();
+        for(int i=0;i<s.size()-1;i++)
+        {
+            if(s[i]!=s[i+1])
+            {
+                ans+=min(i+1,n-1-i);
+            }
+        }
+        return ans;
+    }
+};
